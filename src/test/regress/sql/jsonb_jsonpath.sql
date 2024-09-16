f5ffdc74ea02e3d7dc42ee691617341bb75d7f22
Florents Tselai <florents.tselai@gmail.com>
2024-09-17 00:13:29 +0300

    test case for replace -> starts with

--- src/test/regress/sql/jsonb_jsonpath.sql
+++ src/test/regress/sql/jsonb_jsonpath.sql
@@ -628,7 +628,7 @@ rollback;
 -- select jsonb_path_query('{}', '$.replace("x", "bye")', silent => true);
 select jsonb_path_query('1.23', '$.replace("x", "bye")');
 select jsonb_path_query('"hello world"', '$.replace("hello","bye")');
--- select jsonb_path_query('"hello world"', '$.replace("hello","bye") starts with "bye"');
+select jsonb_path_query('"hello world"', '$.replace("hello","bye") starts with "bye"');
 
 -- Test .time()
 select jsonb_path_query('null', '$.time()');
